3b5e162c374d17a8d5e2072d022e5ebf3083a686
guohu <guohu@renrui.com>
2023-08-21 14:33:46 +0800

    路由组件实例

---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user">
+    <h1>This is an home page</h1>
+    <button @click="goLink('/user')">go user</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+  }
+})
+export default class homeView extends Vue {
+  goLink (val: string) {
+    this.$router.push(val)
+  }
+}
+</script>
---
+++ src/views/user/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="user">
+    <h1>This is an user page</h1>
+    <button @click="countHandle('cut')">-</button>
+    <button>{{ count }}</button>
+    <button @click="countHandle('add')">+</button>
+    <br>
+    <button @click="goLink('/')">go home</button>
+    <br>
+    <HelloWorld msg="This is an helloWord" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class userView extends Vue {
+  count = 0
+  countHandle (type: string) {
+    switch (type) {
+      case 'add':
+        this.count++
+        break
+      case 'cut':
+        this.count--
+        break
+      default:
+        break
+    }
+  }
+
+  goLink (val: string) {
+    this.$router.push(val)
+  }
+}
+</script>
